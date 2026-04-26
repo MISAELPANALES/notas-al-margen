d26f7f917afc591c059da14d9e498d54574ed0b4
Misael Panales <125848263+MISAELPANALES@users.noreply.github.com>
2026-04-26 07:51:44 +0000

    ji

--- notas-al-margen/src/App.jsx
+++ notas-al-margen/src/App.jsx
@@ -11,14 +11,14 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={heroImg} className="base" width="170" height="179" alt="Imagen principal del proyecto" />
+          <img src={reactLogo} className="framework" alt="Logo de React" />
+          <img src={viteLogo} className="vite" alt="Logo de Vite" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Comenzar</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Edita <code>src/App.jsx</code> y guarda para probar <code>HMR</code>
           </p>
         </div>
         <button
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          El contador es {count}
         </button>
       </section>
 
@@ -37,19 +37,19 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <h2>Documentación</h2>
+          <p>Tus preguntas, respondidas</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
+                <img className="logo" src={viteLogo} alt="Logo de Vite" />
+                Explorar Vite
               </a>
             </li>
             <li>
               <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
+                <img className="button-icon" src={reactLogo} alt="Logo de React" />
+                Aprender más
               </a>
             </li>
           </ul>
@@ -58,8 +58,8 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
+          <h2>Conéctate con nosotros</h2>
+          <p>Únete a la comunidad de Vite</p>
           <ul>
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
