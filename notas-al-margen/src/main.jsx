1b6a2775c139b1d4ea33d10a49d4d1a15da6d9a8
Misael Panales <125848263+MISAELPANALES@users.noreply.github.com>
2026-04-26 08:48:31 +0000

    aws instalado y listo

--- notas-al-margen/src/main.jsx
+++ notas-al-margen/src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Amplify } from 'aws-amplify'
 import './index.css'
 import App from './App.jsx'
+
+// Configure Amplify
+Amplify.configure({})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
